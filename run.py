fae7bb388e1fe22e74ea7520bafb045fd28ba5a1
Srz97 <shahraiz97@gmail.com>
2023-06-15 11:39:07 +0000

    update changes

--- run.py
+++ run.py
@@ -5,3 +5,24 @@ from words import words
 guess_word = random.choices(words) 
 
 print(guess_word)
+
+import random
+from words import words
+
+guess_word = random.choices(words) 
+
+print(guess_word)
+
+def display_welcome():
+    print("Welcome to the thrilling world of online hangman!")
+    print("Alright, before we proceed, Could you please grace me with your name")
+
+def get_player_name():
+    name = input("Enter your name: ")
+    if name.isalpha():
+            print(f"Hi {name} let's play Hangman and see if you can crack the code!")
+    else:
+            print("Invalid input. Please enter alphabetic characters only.")
+
+display_welcome()
+get_player_name()
